54388f09f0b3611ab1377036a2c1842ee24b0360
Jason <jasonk822.pv@gmail.com>
2025-08-18 20:41:54 +0100

    Add controls

--- app/graphics/gui/control_panel/control_panel.py
+++ app/graphics/gui/control_panel/control_panel.py
@@ -3,6 +3,7 @@ from ....base.system import System
 from .system_panel import SystemPanel
 from .bodies_panel import BodiesPanel
 from vispy.app import Timer
+from ...utils import utils
 
 
 class ControlPanel(QtWidgets.QWidget):
@@ -14,6 +15,7 @@ class ControlPanel(QtWidgets.QWidget):
 
         self.setObjectName("controlPanel")
         self.setLayout(QtWidgets.QVBoxLayout())
+        self.setStyleSheet(utils.load_css("app/graphics/gui/stylesheets/control_panel.css"))
 
         # system panel label
         self.layout().addWidget(QtWidgets.QLabel("System Properties"))
